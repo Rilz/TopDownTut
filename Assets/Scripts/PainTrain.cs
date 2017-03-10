2dfbf9851f26d62ecf131d5b483d41882c2a57db
Riku <ironzuge1@gmail.com>
2017-03-10 15:36:31 +0200

    säätöö

---
+++ Assets/Scripts/PainTrain.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PainTrain : MonoBehaviour 
+{
+
+	public Entity entity;
+
+	public int damageToGive;
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			entity.health -= damageToGive;
+		}
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+}
